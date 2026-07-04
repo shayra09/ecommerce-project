da4efb20c244f47195ac802968b86cd15edbe989
Shayra <shayra@gmail.com>
2026-07-05 00:49:26 +0530

    Update settings and database

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -35,11 +35,18 @@ import os
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600,
+#     )
+# }
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 ALLOWED_HOSTS = [
